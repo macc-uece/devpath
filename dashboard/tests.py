554ede91529df2700d89f03afd874f908d1a75e9
Pannry <thalestnd@gmail.com>
2018-12-06 20:17:32 -0300

    adding some tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
-
+from django.test import Client
 # Create your tests here.
+
+# Testa se o servidor está funcionando corretamente, sem nenhum problema de execução
+class TestServerRunning(TestCase):
+    '''Alguma descrição'''
+    @classmethod
+    def setUp(self):
+        self.client = Client()
+
+    def test_getIndex(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+# Testa se todas as páginas anterior ao login estao funcionando corretamente
+class TestIfAllPagesRunning(TestCase):
+    '''Alguma descrição'''
+    @classmethod
+    def setUp(self):
+        self.client = Client()
+
+    def test_allPages(self):
+        links = [
+            '/',
+            '/servicos',
+            '/contato',
+            '/user/register',
+            '/user/login'
+        ]
+        for link in links:
+            response = self.client.get(link)
+            self.assertEqual(response.status_code, 200)
