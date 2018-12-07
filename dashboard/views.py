3db179f88154f74559a99e187467e988e32dd67e
Alan Bandeira <alan.p.bandeira@gmail.com>
2018-12-07 14:01:32 -0300

    finishing interface

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from user_manager.models import User, Developer
-from dev_resume.models import Skill
+from dev_resume.models import Skill, Education, Project, Experience
 
 
 # Create your views here.
@@ -22,6 +22,48 @@ def add_skill(request):
         return render(request, 'skills.html', context={'skills': skills})
     else:
         return render(request, 'skills.html')
+
+def add_education(request):
+    if request.method == 'POST':
+        user_name = request.user.get_username()
+        user = User.objects.get(username=user_name)
+        dev = Developer.objects.get(user=user)
+        
+        return render(request, 'education.html')
+    elif request.method == 'GET':
+        eds = Education.objects.all()
+        return render(request, 'education.html', context={'eds': eds})
+    else:
+        return render(request, 'education.html')
+
+def add_experience(request):
+    if request.method == 'POST':
+        user_name = request.user.get_username()
+        user = User.objects.get(username=user_name)
+        dev = Developer.objects.get(user=user)
+        
+        return render(request, 'experiences.html')
+    elif request.method == 'GET':
+        exps = Experience.objects.all()
+        return render(request, 'experiences.html', context={'exps': exps})
+    else:
+        return render(request, 'experiences.html')
+
+def add_project(request):
+    if request.method == 'POST':
+        user_name = request.user.get_username()
+        user = User.objects.get(username=user_name)
+        dev = Developer.objects.get(user=user)
+        
+        return render(request, 'projects.html')
+    elif request.method == 'GET':
+        projects = Project.objects.all()
+        return render(request, 'projects.html', context={'projects': projects})
+    else:
+        return render(request, 'projects.html')
+
+def load_resume(request):
+        return render(request, 'all.html')
 
 def servicos(request):
     return render(request, 'servicos.html')
